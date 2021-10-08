146f9176ab4d5928587a5732f585e2676c656a6b
Parvez <parvezhossen81@gmail.com>
2021-10-08 09:20:22 +0600

    CF106C.py sloved

--- own/CF1506C.py
+++ own/CF1506C.py
@@ -1,9 +1,21 @@
 
-print("lh")
-m=list()
+for i in range(int(input())):
+    str1=input()
+    str2=input()
+    if str1==str2:
+        print(0)
+    else:
 
-a=list()
-for i in range(5):
-    a.append(0)
+        mat=[[0 for i in range(len(str1)+1)] for i in range(len(str2)+1)]
+
+        max=0
+        for i in range(len(str2)):
+            for j in range(len(str1)):
+                if str2[i] == str1[j]:
+                    mat[i+1][j+1]=mat[i][j]+1
+                    if max<mat[i+1][j+1]:
+                        max=mat[i+1][j+1]
+        print(len(str1+str2)-2*max)
+
 
 
