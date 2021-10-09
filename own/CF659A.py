6167895eab999539c127377edf8c9535c86464e8
Parvez <parvezhossen81@gmail.com>
2021-10-09 07:16:34 +0600

    simplified

--- own/CF659A.py
+++ own/CF659A.py
@@ -1,9 +1,6 @@
 n,a,b=map(int,input().split())
 if b<0:
-    if abs(b)-a==0:
-        print(n)
-    else:
-        print(n-(abs(b)-a)%n)
+    print(n-(abs(b)-a)%n)
 else:
     if (a+b)%n==0:
         print(n)
